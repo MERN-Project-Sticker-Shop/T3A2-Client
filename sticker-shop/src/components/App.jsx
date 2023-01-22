b59aae9891a4d4de3b72519cad0710e2fbadabc7
Dandandan-mo <dan.mo6304@gmail.com>
2023-01-22 16:43:50 +1100

    created a react app, copied README

---
+++ sticker-shop/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './Navbar'
+import Contact from './Contact'
+import Home from './Home'
+import Product from './Product'
+
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/product-detail/:id' element={<Product />}/>
+        <Route path='*' element={<h3>Page Not Found!</h3>} />
+      </Routes>
+      <Contact />
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
