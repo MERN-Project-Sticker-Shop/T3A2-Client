4cfa9ef72a79e2560e7d4fc25867414c12a38842
Dandandan-mo <dan.mo6304@gmail.com>
2023-02-02 15:11:54 +1100

    fixed the display of total payable

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import AddedProduct from './AddedProduct'
 
-const Cart = ({cart, setCart, addCartToOrder, setTotal, cartId}) => {
+const Cart = ({cart, setCart, addCartToOrder, total, setTotal, cartId}) => {
 
   const nav = useNavigate()
 
@@ -17,17 +17,16 @@ const Cart = ({cart, setCart, addCartToOrder, setTotal, cartId}) => {
   
   // get an array of all the subtotals
   const subtotals = []
-
-  // sum all the subtotals for the total payable
-  const payable = subtotals.reduce((partialSum, additional) => partialSum + additional, 0)
+  let payable = 0
 
   // When cart is updated, update the total payable
   useEffect(() => {
-    console.log(cart)
     cart.forEach(item => {
       const subtotal = item.price * item.quantity
       subtotals.push(subtotal)
     })
+    payable = subtotals.reduce((partialSum, additional) => partialSum + additional, 0)
+
     setTotal(payable)
   })
 
@@ -51,7 +50,7 @@ const Cart = ({cart, setCart, addCartToOrder, setTotal, cartId}) => {
         {cart.map(item => <div className="card rounded-3 mb-4" key={item.product}><AddedProduct item={item} setCart={setCart} cart={cart}/></div>)}
         <div className="card-body p-4">
             <div className="row d-flex justify-content-between align-items-center">
-                {cart.length > 0 ? <><h4 id="cart-total">Total Payable: ${isNaN(payable)? "--" : payable}</h4><button onClick={toCheckout} className="btn btn-warning btn-block btn-lg">Checkout</button></> : <h4>Your cart is empty.</h4>}
+                {cart.length > 0 ? <><h4 id="cart-total">Total Payable: ${isNaN(total)? "--" : total}</h4><button onClick={toCheckout} className="btn btn-warning btn-block btn-lg">Checkout</button></> : <h4>Your cart is empty.</h4>}
                 <button onClick={toShopping} className="btn btn-primary btn-block btn-lg mt-3">Continue Shopping</button>
             </div>
             
